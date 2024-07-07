0dc9ec6704ed2a2743cfcdc4b7c130d56e5b02e6
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-07 22:48:29 +0530

    overflow fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
+import { useEffect } from 'react';  
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Pnf from './pages/404';
 import Footer from './components/Footer';
 function App() {
+  useEffect(() => {
+  
+
+    document.body.style.overflowX = "hidden";
+
+    return () => {
+      document.body.style.overflowX = "";
+    };
+  }, []);
+
   return (
     <Router>
       <Navbar />
