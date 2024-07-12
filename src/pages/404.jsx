dd5f688b10431580caf7a2824ae4eb7528ce36d8
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-12 20:17:20 +0530

    changes in the working of main page animations

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,6 @@
 export default function Pnf(){
     return(
-        <div className="w-full h-full flex items-center justify-center p-10 flex-col">
+        <div className="w-[100%] h-[100%] flex items-center justify-center p-10 flex-col">
             <h1 className="text-8xl font-bold my-4">404</h1>
 
             <h2 className="text-5xl font-normal text-[#575757]">Page Not Found</h2>
